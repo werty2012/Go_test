d8bf90ee14834262576c4d19302e0a3db39c6d61
yb <werty2012@gmail.com>
2022-11-25 14:38:59 +0300

    added kubernetes.yaml

--- main.go
+++ main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 	ctx := context.Background()
 
 	done := make(chan struct{})
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+
 	s := Server{}
 	s.Run()
 
@@ -16,6 +23,10 @@ func main() {
 		var s string
 		fmt.Scanln(&s)
 		close(done)
+		//sig := <-sigs
+		//fmt.Println()
+		//fmt.Println(sig)
+		//close(done)
 	}()
 
 	<-done
